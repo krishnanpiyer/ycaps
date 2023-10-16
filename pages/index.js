ea184105aa98be5777a98f90ef84a1ecd7f964ec
Diggory Rycroft <diggory.rycroft@vercel.com>
2023-10-16 16:35:34 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,135 +5,41 @@ import map from '../public/map.svg'
 import MyV0Component from './myV0Component'
 import TestComponent from './testComponent'
 
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/wyed8E8
+ */
+import { CardTitle, CardHeader, CardContent, CardFooter, Card } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
 
-// import { Layout } from '@vercel/examples-ui'
-
-// Forward properties from `middleware.ts`
-// When support for configuring gSSP to use Edge Functions lands,
-// We could add that logic here directly.
-export const getServerSideProps = ({ query }) => ({
-  props: query,
-})
-
-export default function Index({
-  name,
-  // languages,
-  city,
-  region,
-  country,
-  cityNickname,
-  // currencyCode,
-  // currencySymbol,
-}) {
-  name = decodeURIComponent(name) || undefined
-  city = decodeURIComponent(city) || undefined
-  country = decodeURIComponent(country) || undefined
-  cityNickname = decodeURIComponent(cityNickname) || undefined
-  let cityWiki = "https://en.wikipedia.org/wiki/"+city+", "+region
+export default function Component() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-50">
-      <div>
-        {true === false ? (
-          <div>
-            <p>Debug:</p>
-            <p>City: ${city} ${typeof(city)}</p>
-            <p>Region: ${region} ${typeof(region)}</p>
-            <p>Country: ${country} ${typeof(country)}</p>
-            <p>City Nickname: ${cityNickname} ${typeof(cityNickname)}</p>
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <Card className="font-mono">
+        <CardHeader className="text-red-500 text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl animate-pulse">
+          <CardTitle>@nextjs</CardTitle>
+        </CardHeader>
+        <CardContent className="text-green-500">The React Framework – created and maintained by @vercel.</CardContent>
+        <CardFooter className="text-blue-500">
+          <div className="flex items-center space-x-4">
+            <Button className="bg-red-500 text-white px-4 py-2 rounded shadow hover:bg-red-600 animate-spin">
+              Red
+            </Button>
+            <Button className="bg-green-500 text-white px-4 py-2 rounded shadow hover:bg-green-600 animate-spin">
+              Green
+            </Button>
+            <Button className="bg-blue-500 text-white px-4 py-2 rounded shadow hover:bg-blue-600 animate-spin">
+              Blue
+            </Button>
+            <Button className="bg-yellow-500 text-white px-4 py-2 rounded shadow hover:bg-yellow-600 animate-spin">
+              Yellow
+            </Button>
+            <Button className="bg-purple-500 text-white px-4 py-2 rounded shadow hover:bg-purple-600 animate-spin">
+              Purple
+            </Button>
           </div>
-          ) : null}
-      </div>
-      <div className="fixed inset-0 overflow-hidden opacity-75 bg-[#f8fafb]">
-        <Image alt="World Map" src={map} fill={true} quality={100} />
-      </div>
-      <main className="flex flex-col items-center flex-1 px-4 sm:px-20 text-center z-10 pt-8 sm:pt-20">
-        <h1 className="text-3xl sm:text-5xl pb-10 font-bold">Welcome!</h1>
-        
-        <MyV0Component />
-
-        {city!=='undefined' ? (
-          <p className="pt-12 text-xl">You're in beautiful {city}, {region}!</p>
-        ) : (
-          <div>
-            <p className="mt-4 pt-10 text-lg sm:text-xl text-gray-700">
-              We'd love to tailor our content to your location, but we haven't set up Edge Middleware yet.
-            </p> 
-            <p className="mt-4 pt-2 text-lg sm:text-xl text-gray-700">
-              <a className="underline text-blue-600" href="https://github.com/dddiggory/ae-cert-next" target="_blank">Help us out?</a>
-            </p>
-          </div>
-        )}
-
-        {city!=='undefined' && cityNickname!=='undefined' ? (
-          <div>
-            <p className="text-xl pb-4">Also known as {cityNickname}.</p>
-            <p>Learn more about {city} <a className="underline text-blue-600" href={cityWiki}>here.</a></p>
-            <p>Want a different {city} nickname? <a href="" className="underline text-blue-600">Refresh the page</a> and Middleware will do the rest.</p>
-            </div>
-        ) : (
-          <p></p>
-        )}
-        
-        {city!=='undefined' && cityNickname==='undefined' ? (
-          <div>
-          <p>I don't know of a nickname for {city}, but I'm sure it's a lovely place.</p>
-          <p>Learn more about {city} <a className="underline text-blue-600" href={cityWiki}>here.</a></p>
-          </div>
-        ) : (
-          <p></p>
-        )}
-    
-
-        
-        {country!=='undefined' ? (
-          <section className="border border-gray-300 bg-white rounded-lg shadow-lg mt-16 w-full hover:shadow-2xl transition">
-          <div className="p-4 flex justify-center items-between border-b">
-            <div className="self-center">
-              <Image
-                alt={`${country} flag`}
-                className="rounded-full"
-                src={`https://flagcdn.com/96x72/${country.toLowerCase()}.png`}
-                // src={`https://flagcdn.com/${country.toLowerCase()}.svg`}
-                width={32}
-                height={32}
-              />
-            </div>
-            <div className="ml-4 mr-auto text-left">
-              {/* <h4 className="font-semibold">{name}</h4> */}
-              <h5 className="text-gray-700">{city}</h5>
-            </div>
-            <p className="self-center text-gray-700">{country}</p>
-          </div>
-          {/* <div className="p-4 flex justify-center items-between border-b bg-gray-50">
-            <h4 className="font-semibold text-left mr-auto">Currency</h4>
-            <p className="text-gray-700">{`${currencyCode} ${currencySymbol}`}</p>
-          </div> */}
-          <div className="p-4 flexborder-b bg-gray-50 rounded-b-lg">
-            <h4 className="font-semibold text-left">Geolocation Headers</h4>
-            <pre className="bg-black text-white font-mono text-left py-2 px-4 rounded-lg mt-4 text-sm leading-6">
-              <p>
-                <strong>{'x-vercel-ip-city: '}</strong>
-                {city}
-              </p>
-              <p>
-                <strong>{'x-vercel-ip-country-region: '}</strong>
-                {region}
-              </p>
-              <p>
-                <strong>{'x-vercel-ip-country: '}</strong>
-                {country}
-              </p>
-            </pre>
-          </div>
-        </section>
-        ) : <div></div>}
-        
-        
-        
-       
-      </main>
+        </CardFooter>
+      </Card>
     </div>
   )
 }
-
-// Index.Layout = Layout
